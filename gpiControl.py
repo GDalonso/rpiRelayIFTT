58ba9825304d979be7ddf13bebe51c6175732655
GDalonso <guicastro.200@gmail.com>
2020-01-15 16:53:13 +0000

    documentação na lib de gpio

--- gpiControl.py
+++ gpiControl.py
@@ -2,11 +2,18 @@ import RPi.GPIO as GPIO
 import sys
 
 def inicializaBoard():
+    # inicializa o gpio da placa
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
 
 def definePinoComoSaida(numeroPino):
+    # define um pino como saída
     GPIO.setup(numeroPino, GPIO.OUT)
 
+def definePinoComoEntrada(numeroPino):
+    # define um pino como entrada
+    GPIO.setup(numeroPino, GPIO.IN)
+
 def escreveParaPorta(numeroPino, estadoPorta):
+    # escreve um estado para uma porta
     GPIO.output(numeroPino, estadoPorta)
