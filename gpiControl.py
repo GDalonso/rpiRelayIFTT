5f6bec8ec58643dd7f89c3b1310da0e88ce0eb2c
GuiDalonso <guicastro.200@gmail.com>
2020-02-02 14:11:18 -0300

    documenting what i did

--- gpiControl.py
+++ gpiControl.py
@@ -1,20 +1,28 @@
 import RPi.GPIO as GPIO
 import sys
 
+
 def inicializaBoard():
     # inicializa o gpio da placa
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
 
+
 def definePinoComoSaida(numeroPino):
     # define um pino como saída
-    # GPIO.cleanup()
+    """
+    O default está high pq os controles do Relay são invertidos, High é desligado e Low é ligado
+    por algum motivo bizarro que ninguém entende
+    Alterar isso pra lidar com qualquer outro equipamento normal
+    """
     GPIO.setup(numeroPino, GPIO.OUT, initial=GPIO.HIGH)
     pass
+
 
 def definePinoComoEntrada(numeroPino):
     # define um pino como entrada
     GPIO.setup(numeroPino, GPIO.IN)
+
 
 def escreveParaPorta(numeroPino, estadoPorta):
     # escreve um estado para uma porta
